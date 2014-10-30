368d5f6f2a6b523c489eda75a17c423cf87d6658
Kevin Solorio <kevin@detone8.com>
2014-10-29 20:36:05 -0400

    implement basic graph api

--- graphs/ruby/bi_graph.rb
+++ graphs/ruby/bi_graph.rb
@@ -5,6 +5,16 @@ class BiGraph
   def initialize(vertices_count: 0)
     @vertices_count = vertices_count
     @edge_count = 0
+    @adjacencies = Array.new(vertices_count) { Array.new }
   end
 
+  def adj(vertex)
+    @adjacencies[vertex]
+  end
+
+  def add_edge(from, to)
+    @edge_count += 1
+    @adjacencies[from].push(to)
+    @adjacencies[to].push(from)
+  end
 end
--- graphs/ruby/spec/bi_graph_spec.rb
+++ graphs/ruby/spec/bi_graph_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe BiGraph do
       specify { expect(subject.vertices_count).to be 27 }
     end
   end
+
+  describe "#adj(vertex)" do
+    subject { BiGraph.new(vertices_count: 10) }
+    context "before edges have been created" do
+      specify { expect(subject.adj(1)).to be_empty }
+    end
+  end
+
+  describe "#add_edge(to, from)" do
+    subject { BiGraph.new(vertices_count: 10) }
+    it "increments the edge count" do
+      expect{subject.add_edge(0,1)}.to change(subject, :edge_count).from(0).to(1)
+    end
+
+    it "adds and adjacent vertex to the 'from' vertex" do
+      expect{subject.add_edge(0, 1)}.to change{subject.adj(0)}.from([]).to([1])
+    end
+
+    it "adds and adjacent vertex to the 'to' vertex" do
+      expect{subject.add_edge(0, 1)}.to change{subject.adj(1)}.from([]).to([0])
+    end
+  end
 end
